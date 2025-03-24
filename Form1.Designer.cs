78f3e9a1add4c4e99abf65bf828f1c57ad3f7417
Hmele_sc73@student.itstep.org <Hmele_sc73@student.itstep.org>
2025-03-24 23:57:31 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FindFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.listBoxResultsDirectories = new System.Windows.Forms.ListBox();
+            this.listBoxResultsFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxResultsFilesInDirectory = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(69, 129);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(407, 31);
+            this.textBoxPath.TabIndex = 0;
+            this.textBoxPath.Text = "путь к папке с файлом";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(69, 205);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(407, 31);
+            this.textBoxFile.TabIndex = 0;
+            this.textBoxFile.Text = "название файла";
+            // 
+            // listBoxResultsDirectories
+            // 
+            this.listBoxResultsDirectories.FormattingEnabled = true;
+            this.listBoxResultsDirectories.ItemHeight = 25;
+            this.listBoxResultsDirectories.Location = new System.Drawing.Point(60, 363);
+            this.listBoxResultsDirectories.Name = "listBoxResultsDirectories";
+            this.listBoxResultsDirectories.Size = new System.Drawing.Size(950, 304);
+            this.listBoxResultsDirectories.TabIndex = 1;
+            // 
+            // listBoxResultsFiles
+            // 
+            this.listBoxResultsFiles.FormattingEnabled = true;
+            this.listBoxResultsFiles.ItemHeight = 25;
+            this.listBoxResultsFiles.Location = new System.Drawing.Point(605, 828);
+            this.listBoxResultsFiles.Name = "listBoxResultsFiles";
+            this.listBoxResultsFiles.Size = new System.Drawing.Size(1058, 304);
+            this.listBoxResultsFiles.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Список вложеных папок";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(972, 783);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Список искомых файлов";
+            // 
+            // listBoxResultsFilesInDirectory
+            // 
+            this.listBoxResultsFilesInDirectory.FormattingEnabled = true;
+            this.listBoxResultsFilesInDirectory.ItemHeight = 25;
+            this.listBoxResultsFilesInDirectory.Location = new System.Drawing.Point(1053, 363);
+            this.listBoxResultsFilesInDirectory.Name = "listBoxResultsFilesInDirectory";
+            this.listBoxResultsFilesInDirectory.Size = new System.Drawing.Size(1018, 304);
+            this.listBoxResultsFilesInDirectory.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1429, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Список файлов в папке";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2144, 1207);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxResultsFilesInDirectory);
+            this.Controls.Add(this.listBoxResultsFiles);
+            this.Controls.Add(this.listBoxResultsDirectories);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.ListBox listBoxResultsDirectories;
+        private System.Windows.Forms.ListBox listBoxResultsFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxResultsFilesInDirectory;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace FindFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxPath.TextChanged += TextBoxPathChanged; // Подписываемся на событие изменения текста
+            textBoxFile.TextChanged += TextBoxFileChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {}
+        private void TextBoxPathChanged (object sender, EventArgs e)
+        {
+            Thread tPath = new Thread(FindDirectory);
+            tPath.IsBackground = false;
+            tPath.Start();
+
+            Thread fPathf = new Thread(FindFile);
+            fPathf.IsBackground = false;
+            fPathf.Start();
+        }
+        private void FindDirectory()
+        {
+            string path = textBoxPath.Text.Trim();
+            listBoxResultsDirectories.Items.Clear(); // Очищаем список перед новым поиском
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    var directories = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
+                    // Добавляем в список
+                    listBoxResultsDirectories.Items.AddRange(directories.ToArray());
+                    if (directories.Length < 1)
+                        listBoxResultsDirectories.Items.Add("Папка не содержит другие папки");
+                }
+                catch (Exception ex)
+                {
+                    listBoxResultsDirectories.Items.Add("Ошибка: " + ex.Message);
+                }
+            }
+            else
+            {
+                listBoxResultsDirectories.Items.Add("Путь не найден");
+            }
+
+        }
+        private void TextBoxFileChanged (object sender, EventArgs e)
+        {
+            Thread fPath = new Thread(FindFileWithParams);
+            fPath.IsBackground = false;
+            fPath.Start();
+
+
+        }
+        private void FindFileWithParams ()
+        {
+            string path = textBoxPath.Text.Trim();
+            string fileName = textBoxFile.Text.Trim();
+            listBoxResultsFiles.Items.Clear(); // Очищаем список перед новым поиском
+            if (Directory.Exists(path))
+            {               
+                try
+                {
+                    
+                    var files = Directory.GetFiles(path, fileName, SearchOption.AllDirectories);
+                    // Добавляем в список
+                    listBoxResultsFiles.Items.AddRange(files.ToArray());
+                    if (files.Length < 1)
+                    listBoxResultsFiles.Items.Add("Файл не найден");
+
+
+                }
+                catch (Exception ex)
+                {
+                    listBoxResultsFiles.Items.Add("Ошибка: " + ex.Message);
+                }
+            }
+            else
+            {
+                listBoxResultsFiles.Items.Add("Путь к файлу не найден");
+            }
+
+        }
+        //++++++++++++++++++++++++++++++++++++++++++++++
+        private void FindFile ()
+        {
+            string path = textBoxPath.Text.Trim();
+            listBoxResultsFilesInDirectory.Items.Clear(); // Очищаем список перед новым поиском
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+                    // Добавляем в список
+                    listBoxResultsFilesInDirectory.Items.AddRange(files.ToArray());
+                    if (files.Length < 1)
+                        listBoxResultsFiles.Items.Add("В папке нет файлов");
+
+
+                }
+                catch (Exception ex)
+                {
+                    listBoxResultsFilesInDirectory.Items.Add("Ошибка: " + ex.Message);
+                }
+            }
+            else
+            {
+                listBoxResultsFilesInDirectory.Items.Add("Путь к файлу не найден");
+            }
+        }
+
+
+    }
+}
+
